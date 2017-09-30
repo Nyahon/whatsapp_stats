8f9995e4edf72a0a826ab8f7c952d72b69807a66
nyahon <yohann.meyer@gmail.com>
2017-09-30 21:49:33 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/projects.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projects.dir/main.cpp.o"
+  "projects.pdb"
+  "projects"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,45 @@
+// reading a text file
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+const uint NAME_WIDTH_MAX = 10;
+int main () {
+    string line;
+    ifstream myfile ("../anna.txt");
+    string user1 = "Yohann";
+    string user2 = "Annaline";
+    uint numMessUser1 = 0;
+    uint numMessUser2 = 0;
+
+    if (myfile.is_open())
+    {
+        while ( getline (myfile,line) )
+        {
+            //cout << line << '\n';
+            if (line.find(user1) != string::npos) {
+                numMessUser1++;
+            }
+            if (line.find(user2) != string::npos) {
+                numMessUser2++;
+            }
+
+        }
+        myfile.close();
+
+        cout << setw(NAME_WIDTH_MAX) << left << user1 << ": " << numMessUser1 << " msgs /"
+             << (double)numMessUser1/(numMessUser1+numMessUser2)*100  << "%" << endl;
+        cout << setw(NAME_WIDTH_MAX) <<         user2 << ": " << numMessUser2 << " msgs /"
+             << (double)numMessUser2/(numMessUser1+numMessUser2)*100  << "%" << endl;
+
+
+    }
+
+    else cout << "Unable to open file";
+
+    return 0;
+}
